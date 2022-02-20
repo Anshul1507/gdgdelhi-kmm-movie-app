80adcc0b0b1b027ed1bf8518e4af7e77cc6349a2
Anshul1507 <ag1507anshul@gmail.com>
2022-02-20 22:53:51 +0530

    added api client for movie details api

--- shared/src/commonMain/kotlin/com/github/anshul1507/kmm_network_request/repo/ModelRemoteClient.kt
+++ shared/src/commonMain/kotlin/com/github/anshul1507/kmm_network_request/repo/ModelRemoteClient.kt
@@ -1,6 +1,7 @@
 package com.github.anshul1507.kmm_network_request.repo
 
 import com.github.anshul1507.kmm_network_request.httpClient
+import com.github.anshul1507.kmm_network_request.model.MovieDetails
 import com.github.anshul1507.kmm_network_request.model.MovieResponse
 import io.ktor.client.request.*
 
@@ -8,8 +9,14 @@ import io.ktor.client.request.*
 //Make your own API_KEY from https://www.themoviedb.org/settings/api
 const val API_KEY = "910cb471f3326152066529eef1b406b2"
 const val requestUrl = "https://api.themoviedb.org/3/movie/popular?api_key=$API_KEY"
+var movieDetailUrl = "https://api.themoviedb.org/3/movie/"
 const val posterBaseUrl = "https://image.tmdb.org/t/p/original/"
 
 suspend fun getPopularMovies(): MovieResponse {
     return httpClient.get(requestUrl)
+}
+
+suspend fun getMovieDetail(movieId: Int): MovieDetails {
+    movieDetailUrl += "${movieId}?api_key=${API_KEY}"
+    return httpClient.get(movieDetailUrl)
 }
