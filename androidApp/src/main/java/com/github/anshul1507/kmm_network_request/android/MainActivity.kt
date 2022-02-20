a5619b580e130ee917f3ab2e497294ed80228625
Anshul1507 <ag1507anshul@gmail.com>
2022-02-20 21:52:36 +0530

    update UI layouts

--- androidApp/src/main/java/com/github/anshul1507/kmm_network_request/android/MainActivity.kt
+++ androidApp/src/main/java/com/github/anshul1507/kmm_network_request/android/MainActivity.kt
@@ -2,14 +2,8 @@ package com.github.anshul1507.kmm_network_request.android
 
 import android.os.Bundle
 import android.util.Log
-import android.widget.TextView
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import com.github.anshul1507.kmm_network_request.Greeting
-
-fun greet(): String {
-    return Greeting().greeting()
-}
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,9 +12,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val tv: TextView = findViewById(R.id.text_view)
-        tv.text = greet()
 
         viewModel.getPopularMovies().observe(this) {
             if (it.isEmpty())
