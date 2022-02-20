30fc16222560f15e65e61a40b103c16ff8a77ef6
Anshul1507 <ag1507anshul@gmail.com>
2022-02-20 22:55:10 +0530

    added UI for movie details

--- androidApp/src/main/java/com/github/anshul1507/kmm_network_request/android/movie_details/MovieDetailsActivity.kt
+++ androidApp/src/main/java/com/github/anshul1507/kmm_network_request/android/movie_details/MovieDetailsActivity.kt
@@ -1,6 +1,22 @@
-package com.github.anshul1507.kmm_network_request.android
+package com.github.anshul1507.kmm_network_request.android.movie_details
 
+import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import com.github.anshul1507.kmm_network_request.android.databinding.ActivityMovieDetailsBinding
 
 class MovieDetailsActivity: AppCompatActivity() {
+
+    private val viewModel: MovieDetailViewModel by viewModels()
+
+    private val binding by lazy {
+        ActivityMovieDetailsBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+    }
+
 }
